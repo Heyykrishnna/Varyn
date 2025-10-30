9cc532ef69c9ee85a285dcba11c0827cc93c1b84
Yatharth <khandelwalyatharth39@gmail.com>
2025-10-30 23:50:33 +0530

    Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ export default function Hero() {
       <div className="absolute inset-0">
         <video
           ref={videoRef}
-          src="/GAMETRAILER.mp4"
+          src="/GAMETRAILERF.mp4"
           autoPlay
           loop
           muted={muted}
