cdf6601978b2588dffbcc9948c0115bca10689d4
Yatharth <khandelwalyatharth39@gmail.com>
2025-11-10 12:10:06 +0530

    Colour changed

--- src/components/CardNav.jsx
+++ src/components/CardNav.jsx
@@ -155,7 +155,7 @@ const CardNav = ({
       <nav
         ref={navRef}
         className={`card-nav ${isExpanded ? 'open' : ''} block h-[60px] p-0 rounded-xl shadow-md relative overflow-hidden will-change-[height]`}
-        style={{ backgroundColor: '#98989c' }}>
+        style={{ backgroundColor: '#9E9E9E' }}>
         <div
           className="card-nav-top absolute inset-x-0 top-0 h-[60px] flex items-center justify-between p-2 pl-[1.1rem] z-[2]">
           <div
